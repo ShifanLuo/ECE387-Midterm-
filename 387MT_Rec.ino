80637439efc30bda177225650e6a220972571080
ShifanLuo <33041087+ShifanLuo@users.noreply.github.com>
2018-03-13 01:27:30 -0400

    Add files via upload

---
+++ 387MT_Rec.ino
@@ -0,0 +1,33 @@
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "00001";
+const int buzzer =5;
+void setup() {
+  pinMode(buzzer, OUTPUT);
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+void loop() {
+  if (radio.available()) {
+    int text;
+    radio.read(&text, sizeof(text));
+    if(text == 1){
+       
+  tone(buzzer, 1000); // Send 1KHz sound signal...
+  delay(200);        // ...for 1 sec
+  noTone(buzzer);     // Stop sound...
+  delay(200);        // ...for 1sec
+  
+       Serial.println(text);
+    }else{
+       Serial.println(text);
+    }
+   
+  }
+}
---
+++ 387MT_Tran.ino
@@ -0,0 +1,37 @@
+
+const int out=3;
+const int in=4;
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "00001";
+int SentMessage = 0;
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+  pinMode(out,OUTPUT);
+  pinMode(in,INPUT);
+}
+void loop() {
+  const int TC = 1;
+   const int TF = 0;
+  long dur;
+  digitalWrite(out,LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(out,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(out,LOW);
+  dur=pulseIn(in,HIGH);
+  delay(200);
+   if(dur <500){
+    radio.write(&TC, sizeof(TC));
+  }else{
+   radio.write(&TF, sizeof(TF));
+  }
+ delay(1000);
+  
+}
